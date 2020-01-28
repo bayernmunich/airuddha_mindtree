290a8a62454ad5fe15d70469bb2b0df011228ca4
bayernmunich <aniruddhajere@outlook.com>
2020-01-28 11:30:25 +0530

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -31,16 +31,16 @@ if($text -match "Create Table")
 Write-Host "Create Table does not exists in sql scripts"
 }
 ##############################################################################################################
-$text=Get-Content "D:\a\1\s\*\**.sql" | Select-String  "Drop Table"
-write-host $text
-if($text -match "Drop Table")
-{
- Write-Error "$("contains alias") $($env:ErrorMessage)"
-}
-else
-{
-Write-Error "Drop Table does exists in sql scripts"
-}
+#$text=Get-Content "D:\a\1\s\*\**.sql" | Select-String  "Drop Table"
+#write-host $text
+#if($text -match "Drop Table")
+#{
+# Write-Error "$("contains alias") $($env:ErrorMessage)"
+#}
+#else
+#{
+#Write-Error "Drop Table does exists in sql scripts"
+#}
 ##############################################################################################################
 $text=Get-Content "D:\a\1\s\*\**.sql" | Select-String  "TRUNCATE TABLE ##"
 write-host $text
@@ -57,14 +57,14 @@ Write-Host "Drop Table ##  exists in sql scripts"
 
 }
 ##############################################################################################################
-$text=Get-Content "D:\a\1\s\*\**.sql" | Select-String  "TRUNCATE TABLE"
-write-host $text
-if($text -match "TRUNCATE TABLE")
-{
- Write-Error "$("contains alias") $($env:ErrorMessage)"
-}
-else
-{
-Write-Error "Truncate Table exists in sql scripts"
-}
+#$text=Get-Content "D:\a\1\s\*\**.sql" | Select-String  "TRUNCATE TABLE"
+#write-host $text
+#if($text -match "TRUNCATE TABLE")
+#{
+# Write-Error "$("contains alias") $($env:ErrorMessage)"
+#}
+#else
+#{
+#Write-Error "Truncate Table exists in sql scripts"
+#}
 ##############################################################################################################
